7e1c0db5aad020226f1b93e9fb46010f008a34cd
Steven Au <25945183+thesteau@users.noreply.github.com>
2020-03-25 23:33:18 -0700

    Create File_Extension_Changer.py
    General purpose personally reusable code for import.

---
+++ File_Extension_Changer.py
@@ -0,0 +1,33 @@
+# By Steven Au
+
+import pathlib as pl
+import platform as pf
+
+def pathing_library_mojo(some_file, file_convert='csv'):
+    """ Parse the file string and extract all necessawry elements to breakdown and conert a file to a desired format name.
+        Note: This function's purposes is only taking an input file and return the original path and newly converted file path
+        Parameters:
+            OPTIONAL File_convert
+                csv is the default file extension. For any other file extensions, just add what is needed
+                Note:
+                    Do not add a "." to any desired file extension when using this function
+        Returns:
+            orig_file_path:
+                The direct file path like "c:"
+            new_export:
+                This is the new file name - default is CSV
+        You need to import two standard libraries: pathlib and platform
+            pathlib is the bread and butter of the function
+            platform is just to check the operating system and perform a simple conversion
+    """
+    orig_file_path = pl.Path(some_file)
+    parent_path = orig_file_path.parent
+    new_filename = orig_file_path.stem + '.' + file_convert
+
+    directory_pathing = '/'
+    if pf.system().lower() == 'windows':
+        directory_pathing = '\\'
+
+    new_export = parent_path + directory_pathing + new_filename
+
+    return orig_file_path, new_export
